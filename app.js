a2a911a11da8699c38b9301e7a0c63d296f134d2
eduardoequis <eduardosanchez01@gmail.com>
2020-05-26 00:17:12 -0300

    Express added

--- app.js
+++ app.js
@@ -84,9 +84,6 @@ console.log("Started Online and good!")
 
 // Heroku config add.
 
-var http = require('http'); http.createServer(function (req, res) 
-{ res.writeHead(200, {'Content-Type': 'text/plain'}); 
-res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 
 // Heroku wake up:
 
@@ -94,7 +91,7 @@ let timeInterval = 30 * 60000 // 30 minutes
 
 setInterval(() => {
    
-    console.log(console.log("I'm online"))
+    console.log("I'm online")
 
 }, timeInterval) 
 
